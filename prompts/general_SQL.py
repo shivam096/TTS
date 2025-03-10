3b69339f1faefdc0faf846efd525f616e69064ba
Shivam <shivam06@uw.edu>
2025-03-09 17:17:19 -0700

    Update general Prompt

--- prompts/general_SQL.py
+++ prompts/general_SQL.py
@@ -11,7 +11,7 @@ Please help to generate a SQL query to answer the question. Follow the response 
 2. If the general query can be generated, please generate a valid query without any explanations for the question.
 3. If the general query can't be generated, please explain why it can't be generated.
 4. Please format the query before responding.
-6. Please do not include any string like "```json...```" in the response.
+6. Please DO NOT include any string like "```json...```" in the response.
 7. Pease make sure to return FALSE if the query can't be generated.
 8. Please always respond with a valid well-formed JSON output but without any additional text. 
 
